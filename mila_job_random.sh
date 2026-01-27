346c8cd93411f7dcb0b3ac5e53677d9fe70b9a1e
Scezquer <scezaquer@gmail.com>
2026-01-27 14:45:57 -0500

    updates

--- mila_job_random.sh
+++ mila_job_random.sh
@@ -11,4 +11,4 @@ module load python/3.10
 source $HOME/ENV/bin/activate
 export HF_HUB_CACHE=$SCRATCH/HF-cache
 export UNSLOTH_CACHE_DIR=$SLURM_TMPDIR/unsloth-cache
-python src/main_unsloth.py --loras_path $SCRATCH/qwen-loras --random_graph
+python src/main_unsloth.py --loras_path $SCRATCH/qwen-loras --random_graph --survey_output survey_random.json
