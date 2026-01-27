8b11f5a530821bc6d923114635080008f6e49499
Scezquer <scezaquer@gmail.com>
2026-01-27 12:57:03 -0500

    separate job files for both setups

--- mila_job_random.sh
+++ mila_job_random.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=SimFinetune
+#SBATCH --job-name=RandomGraph
 #SBATCH --array=0
 #SBATCH --time=7:00:00
 #SBATCH --mem=24Gb
@@ -10,4 +10,4 @@
 module load python/3.10
 source $HOME/ENV/bin/activate
 export HF_HUB_CACHE=$SCRATCH/HF-cache
-python src/main.py --loras_path $SCRATCH/qwen-loras
+python src/main.py --loras_path $SCRATCH/qwen-loras --random_graph
