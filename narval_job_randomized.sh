1009b99fc429329cf27aaa9c09276a80b009cbc7
Scezquer <scezaquer@gmail.com>
2026-03-25 18:09:48 -0400

    updates

--- narval_job_randomized.sh
+++ narval_job_randomized.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=def-rrabba
 #SBATCH --job-name=RandomizedSim
-#SBATCH --array=0-75
+#SBATCH --array=0-200
 #SBATCH --time=10:00:00
 #SBATCH --mem-per-cpu=24G
 #SBATCH --gpus-per-node=a100:1
