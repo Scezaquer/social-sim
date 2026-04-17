1724ca22b604db11bf10aa084667aa1627cb4fdb
Scezquer <scezaquer@gmail.com>
2026-04-17 10:41:02 -0400

    updates

--- bash_job_scripts/narval_job_comp550.sh
+++ bash_job_scripts/narval_job_comp550.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=ctb-liyue
 #SBATCH --job-name=Comp550BaseOnly
-#SBATCH --array=318
+#SBATCH --array=318,303,431,591,427,576,590,482,384
 #SBATCH --time=3:00:00
 #SBATCH --mem-per-cpu=24G
 #SBATCH --gpus-per-node=a100:1
