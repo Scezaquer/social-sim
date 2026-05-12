cdecacfe41b5c87e8e2863178b6f05f42d9341a0
Scezquer <scezaquer@gmail.com>
2026-05-12 17:19:20 -0400

    updates

--- bash_job_scripts/emnlp2026/mila_randomized_network.sh
+++ bash_job_scripts/emnlp2026/mila_randomized_network.sh
@@ -3,9 +3,8 @@
 #SBATCH --array=1-150
 #SBATCH --time=6:00:00
 #SBATCH --mem=24Gb
-#SBATCH --gres=gpu:rtx8000:1
+#SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=1
-#SBATCH --partition=unkillable
 
 
 set -euo pipefail
