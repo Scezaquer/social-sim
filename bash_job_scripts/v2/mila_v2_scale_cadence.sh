c706a5057845332eec286916125d4c8c961d0b94
Scezquer <scezaquer@gmail.com>
2026-06-14 19:42:53 -0400

    updates

--- bash_job_scripts/v2/mila_v2_scale_cadence.sh
+++ bash_job_scripts/v2/mila_v2_scale_cadence.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=V2Scale
 #SBATCH --array=0-71
-#SBATCH --time=48:00:00
+#SBATCH --time=24:00:00
 #SBATCH --mem=24Gb
 #SBATCH --gres=gpu:rtx8000:1
 #SBATCH --cpus-per-task=1
