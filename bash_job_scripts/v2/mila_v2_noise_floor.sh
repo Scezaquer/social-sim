42cdddded6b5900993786df2b5b47ce2d124c360
Scezquer <scezaquer@gmail.com>
2026-06-17 14:27:36 -0400

    updates

--- bash_job_scripts/v2/mila_v2_noise_floor.sh
+++ bash_job_scripts/v2/mila_v2_noise_floor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=V2Noise
 #SBATCH --array=0-95
-#SBATCH --time=6:00:00
+#SBATCH --time=24:00:00
 #SBATCH --mem=24Gb
 #SBATCH --gres=gpu:rtx8000:1
 #SBATCH --cpus-per-task=1
