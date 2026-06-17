7fae88752334a49aaa6226c63f142179897310e4
Scezquer <scezaquer@gmail.com>
2026-06-17 14:27:51 -0400

    updates

--- bash_job_scripts/v2/mila_v2_topology.sh
+++ bash_job_scripts/v2/mila_v2_topology.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=V2Topology
 #SBATCH --array=0-335
-#SBATCH --time=12:00:00
+#SBATCH --time=24:00:00
 #SBATCH --mem=24Gb
 #SBATCH --gres=gpu:rtx8000:1
 #SBATCH --cpus-per-task=1
